b69232c165241bbf43d38b5cb26cc7f8601feef2
Marina.S. <marina.sevostian@gmail.com>
2025-03-24 05:13:33 +0200

    add one contact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,17 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  try {
+    const contacts = await readContacts();
+    const newContact = createFakeContact();
+    contacts.push(newContact);
+    await writeContacts(contacts);
+    return contacts;
+  } catch (error) {
+    console.error('Помилка при генерації контактів:', error.massage);
+  }
+};
 
 addOneContact();
