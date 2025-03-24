e12603ab546502cda89f03ce47ce5854cad38031
Marina.S. <marina.sevostian@gmail.com>
2025-03-24 05:34:18 +0200

    add count contacts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,16 @@
-export const countContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const countContacts = async () => {
+  try {
+    const contacts = await readContacts();
+    if (!contacts) {
+      return;
+    }
+    const lengthContacts = contacts.length;
+    return lengthContacts;
+  } catch (error) {
+    console.error('Помилка при генерації контактів:', error.message);
+  }
+};
 
 console.log(await countContacts());
