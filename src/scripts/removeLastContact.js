82bc8324d1c021f867c9748a0b880d64b5a9c6e2
Marina.S. <marina.sevostian@gmail.com>
2025-03-24 06:22:32 +0200

    remove last contact

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,18 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+    if (!contacts) {
+      return;
+    }
+    const lengthContacts = contacts.length;
+    const newArray = contacts.slice(0, lengthContacts - 1);
+    await writeContacts(newArray);
+  } catch (error) {
+    console.error('Помилка при генерації контактів:', error.message);
+  }
+};
 
 removeLastContact();
