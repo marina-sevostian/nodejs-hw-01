10a2f28679d884b6064b821f29308e9184d53b06
Marina.S. <marina.sevostian@gmail.com>
2025-03-24 03:57:05 +0200

    add constant and utils

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, { encoding: 'utf-8' });
+    const contscts = JSON.parse(data);
+    return contscts;
+  } catch (error) {
+    console.error('Помилка при генерації контактів:', error.message);
+  }
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const data = await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(updatedContacts, undefined, 2),
+    );
+    return data;
+  } catch (error) {
+    console.error('Помилка при генерації контактів:', error.massage);
+  }
+};
