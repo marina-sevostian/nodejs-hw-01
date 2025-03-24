6c3ec61639e3c3bda5c39d6bd453950daea05678
Marina.S. <marina.sevostian@gmail.com>
2025-03-24 04:40:23 +0200

    add generateContacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,22 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  try {
+    let contacts = await readContacts();
+    if (!Array.isArray(contacts)) {
+      contacts = [];
+    }
+    for (let i = 0; i < number; i++) {
+      const newContacts = createFakeContact();
+      contacts.push(newContacts);
+    }
+    await writeContacts(contacts);
+    return contacts;
+  } catch (error) {
+    console.error('Помилка при генерації контактів:', error.message);
+  }
+};
 
 generateContacts(5);
